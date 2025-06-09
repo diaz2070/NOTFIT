472ab3b344acd1682061144fd0ba83e8091d28cf
Andres <andres.diazchavarria@ucr.ac.cr>
2025-06-09 02:28:02 -0600

    refactor(auth): add sign-in page and auth page component
    - Create sign-in page component
    - Update sign-up page
    - Add reusable AuthPage component for consistent UI

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,25 +1,11 @@
-import AuthForm from '@/components/auth/AuthForm';
-import {
-  Card,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
+import AuthPage from '@/components/auth/AuthPage';
 
 export default function SignUp() {
   return (
-    <div className="mt-20 flex flex-1 flex-col items-center">
-      <Card className="w-full max-w-md">
-        <CardHeader className="mb-4 text-center">
-          <CardTitle className="text-3xl font-[family-name:var(--font-lemon)]">
-            Sign Up
-          </CardTitle>
-          <CardDescription>
-            Enter your email below to sign in into your gym
-          </CardDescription>
-        </CardHeader>
-        <AuthForm />
-      </Card>
-    </div>
+    <AuthPage
+      title="Sign Up"
+      description="Enter your information to create an account with us!"
+      type="sign-up"
+    />
   );
 }
---
+++ src/components/auth/AuthPage.tsx
@@ -0,0 +1,35 @@
+import AuthForm from '@/components/auth/AuthForm';
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+
+type AuthPageProps = Readonly<{
+  title: string;
+  description: string;
+  type: 'sign-in' | 'sign-up';
+}>;
+
+function AuthPage({
+  title = 'Auth Page',
+  description = 'Please sign in or sign up to continue',
+  type = 'sign-in',
+}: AuthPageProps) {
+  return (
+    <div className="mt-20 flex flex-1 flex-col items-center">
+      <Card className="w-full max-w-md">
+        <CardHeader className="mb-4 text-center">
+          <CardTitle className="text-3xl font-[family-name:var(--font-lemon)]">
+            {title}
+          </CardTitle>
+          <CardDescription>{description}</CardDescription>
+        </CardHeader>
+        <AuthForm type={type} />
+      </Card>
+    </div>
+  );
+}
+
+export default AuthPage;
