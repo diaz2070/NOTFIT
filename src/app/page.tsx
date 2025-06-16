e6b069a901b19453527ba04d324f1b8918f8eeb4
Andres <andres.diazchavarria@ucr.ac.cr>
2025-06-16 13:00:11 -0600

    fix(homepage): small change por consistency homepage images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -67,7 +67,7 @@ export default function Home() {
               alt="Fitness Hero Image"
               width={600}
               height={400}
-              className="hidden dark:block "
+              className="block dark:hidden w-50 h-auto sm:w-70 sm:h-auto md:w-100 md:h-auto object-fit"
             />
             <Image
               src="/logo-light.svg"
